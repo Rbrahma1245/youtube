d8b40e47dd545389b496ac9c88445973985e256f
Rbrahma1245 <rbrahma1245@gmail.com>
2024-02-01 10:51:21 +0530

    added toast message

--- src/Components/InputField.jsx
+++ src/Components/InputField.jsx
@@ -7,7 +7,10 @@ import {
 import { AvatarGenerator } from "random-avatar-generator";
 import { v4 as uuidv4 } from "uuid";
 
-import "./Display.scss"
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import "./Display.scss";
 
 export class InputField extends Component {
   constructor() {
@@ -22,18 +25,18 @@ export class InputField extends Component {
   }
   handleSubmit() {
     if (this.state.inputText == "") {
-      alert("please add the comment");
+      toast.error("please add your comment.");
     } else {
       const uniqueId = uuidv4(); // Generate a new unique ID
 
       let generator = new AvatarGenerator();
-      let name = uniqueNamesGenerator({  
+      let name = uniqueNamesGenerator({
         dictionaries: [adjectives, names],
         separator: " ",
         style: "capital",
       });
       const randomImage = generator.generateRandomAvatar(name); // Generate a new random image
-      let date = new Date()
+      let date = new Date();
 
       this.props.addComment({
         id: uniqueId.slice(0, 8),
@@ -43,20 +46,29 @@ export class InputField extends Component {
         image: randomImage,
       });
       this.setState({ inputText: "", uniqueId: "" });
+
+      toast.success("Commented successful.");
     }
   }
 
   render() {
     return (
       <div>
-        <input 
-        className="text-field"
+        <ToastContainer position="top-center" autoClose={2000} />
+        <input
+          className="text-field"
           type="text"
           placeholder="Add a comment..."
           value={this.state.inputText}
           onChange={this.handleChange.bind(this)}
         />
-        <button className="add-comment-btn" onClick={this.handleSubmit.bind(this)}>Add Comment</button>
+
+        <button
+          className="add-comment-btn"
+          onClick={this.handleSubmit.bind(this)}
+        >
+          Add Comment
+        </button>
       </div>
     );
   }
